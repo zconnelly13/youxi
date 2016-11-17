de7dcdcdd1c0c836b216c84350f496fab04aaeaa
Zac Connelly <zconnelly13@gmail.com>
2016-11-17 12:53:09 -0800

    poll for file changes in lib

--- py/runserver.py
+++ py/runserver.py
@@ -1,18 +1,24 @@
-from os import listdir
 import SimpleHTTPServer
-from settings import constants
 import SocketServer
+from multiprocessing import Process
+from os import listdir
+from settings import constants
+import time
 
 
-def compile_lib():
-    path = "lib"
+def get_file_names(path):
     file_names = [f for f in listdir(path)
                   if any([
                     f.endswith(".js"),
                     f.endswith(".html"),
                     f.endswith(".css")]) and
                   not f.startswith("compiled_")]
-    for f in file_names:
+    return file_names
+
+
+def compile_lib():
+    path = "lib"
+    for f in get_file_names(path):
         file_path = "%s/%s" % (path, f)
         with open(file_path, 'r') as fp:
             content = fp.read()
@@ -21,6 +27,26 @@ def compile_lib():
         new_file = "%s/compiled_%s" % (path, f)
         with open(new_file, 'w') as fp:
             fp.write(content)
+
+
+def get_file_dict():
+    path = "lib"
+    files = {}
+    for f in get_file_names(path):
+        file_path = "%s/%s" % (path, f)
+        with open(file_path, 'r') as fp:
+            files[file_path] = fp.read()
+    return files
+
+
+def listen_for_changes(name):
+    files = get_file_dict()
+    while True:
+        time.sleep(2)
+        new_files = get_file_dict()
+        if files != new_files:
+            compile_lib()
+            files = new_files
 
 
 def serve():
@@ -34,4 +60,6 @@ def serve():
     httpd.serve_forever()
 
 compile_lib()
+p = Process(target=listen_for_changes, args=('bob',))
+p.start()
 serve()
